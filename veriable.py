e5a9844d16d92dcc22e5d9f1021713a4733d2595
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-17 22:30:13 +0600

    variables type

---
+++ veriable.py
@@ -0,0 +1,29 @@
+# veriable
+# variable is a container for storing data
+# variable can be of any data type
+# variable can be of any name
+# variable can be of any length
+# variable can be of any case
+# variable can be of any number
+ 
+
+# integer 
+a = 5
+print(a)
+
+# string
+b = "tasin"
+print(b)
+
+# float
+c = 1.5
+print(c)
+
+# boolean
+d = True
+print(d)
+
+# complex
+e = 1j
+print(e)
+
