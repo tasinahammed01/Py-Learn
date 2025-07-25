1e898490b223f97e6b3da23bc8df97a94af2be0d
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-25 23:58:14 +0600

    string related question solved

--- Practice3_String.py
+++ Practice3_String.py
@@ -1,17 +1,55 @@
 # 1. Write a python program to display a user entered name followed by Good Afternoon using input () function.
 
-name = input("Enter your name:")
+# name = input("Enter your name:")
 
-print("Good Afternoon", name)
+# print("Good Afternoon", name)
 
+# --------------------------------------------------------------
 
 
 # 2. Write a program to fill in a letter template given below with name and date.
-# letter = **•
+# letter = ***
 # Dear < Name|›,
 # You are selected!
 # < |Date| >
+
+
+# letter = ''' Dear |Name|,
+#              You are selected!
+#              |Date|'''
+
+# print(letter.replace("|Name|" , "Tasin").replace("|Date|" , "01 July 2025"))
+
+
+
+
+# --------------------------------------------------------------
+
+
 # 3. Write a program to detect double space in a string.
+
+
+# text = "My  name is  Tanjid ahammed  Tasin"
+
+# print(text.find("  "))
+
+
+# --------------------------------------------------------------
+
 # 4. Replace the double space from problem 3 with single spaces.
-# 5. Write a program to format the following letter using escape sequence
-# characters.
+
+
+
+
+# text = "My  name is  Tanjid ahammed  Tasin"
+
+# print(text.replace("  ", " "))
+
+
+
+
+# --------------------------------------------------------------
+
+# 5. Write a program to format the following letter using escape sequence characters.
+# Letter = " Dear Tasin, this python is nice. Thanks !"
+
