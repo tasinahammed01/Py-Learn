a595f554f5add6a04fcc8dfdc7953f4eee396da8
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-29 22:59:29 +0600

    method learned

--- Tuple.py
+++ Tuple.py
@@ -19,3 +19,18 @@ print(d[0])
 
 # d[0] = 100
 # print(d)  can't change the value of tuple. tuple is immutable
+
+
+
+# =======================================================
+
+
+# Methods of tuple
+
+abc = (1,2,3,2,2, "Tasin", "Tanjid", False)
+
+print(abc.count(2))
+
+
+i = abc.index(2) # it will return the index of 2
+print(i)
