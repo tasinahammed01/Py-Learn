e6ce8c6edc9b51744752f62d1d90c11dcc2022ee
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-29 22:39:25 +0600

    how to declare single tuples

--- Tuple.py
+++ Tuple.py
@@ -5,3 +5,10 @@
 a = (1,2,3,4)  
 print(type(a))
 
+# write single value in tuple
+
+b = (1) #This is not a tuple
+print(type(b))
+
+c = (1,) #This is a tuple. whe have to declare single value with comma
+print(type(c))
