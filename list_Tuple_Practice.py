7dd4c18f5f62b78d33092affe8728e08ed4508c9
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-30 23:48:07 +0600

    1st problem solved

--- list_Tuple_Practice.py
+++ list_Tuple_Practice.py
@@ -1,4 +1,24 @@
 # 1. Write a program to store seven fruits in alist entered by the user.
+
+fruits = []
+
+f1 = input("Enter Fruit name:")
+
+fruits.append(f1)
+
+print(fruits)
+
+
+
+
+# =======================================================================
+
+
+
+
+# =======================================================================
+
+
 # 2. Write a program to accept marks of 6 students and display them in a sorted
 # manner.
 # 3. Check that a type cannot be changed in python.
