08c63468eedd5bf18d7cef9596f85dfd5c35bedc
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-31 01:44:43 +0600

    problem 3 completed

--- list_Tuple_Practice.py
+++ list_Tuple_Practice.py
@@ -1,25 +1,20 @@
 # 1. Write a program to store seven fruits in alist entered by the user.
 
-fruits = []
+# fruits = []
 
-f1 = input("Enter Fruit 1st name:")
-fruits.append(f1)
+# f1 = input("Enter Fruit 1st name:")
+# fruits.append(f1)
 
-f2 = input("Enter Fruit 2nd name:")
-fruits.append(f2)
+# f2 = input("Enter Fruit 2nd name:")
+# fruits.append(f2)
 
-f3 = input("Enter Fruit 3rd name:")
-fruits.append(f3)
+# f3 = input("Enter Fruit 3rd name:")
+# fruits.append(f3)
 
-f4 = input("Enter Fruit 4th name:")
-fruits.append(f4)
+# f4 = input("Enter Fruit 4th name:")
+# fruits.append(f4)
 
-print(fruits)
-
-
-
-
-# =======================================================================
+# print(fruits)
 
 
 
@@ -27,9 +22,53 @@ print(fruits)
 # =======================================================================
 
 
-# 2. Write a program to accept marks of 6 students and display them in a sorted
-# manner.
-# 3. Check that a type cannot be changed in python.
+
+
+# =======================================================================
+
+
+# 2. Write a program to accept marks of 6 students and display them in a sorted manner.
+
+
+
+# marks = []
+
+# f1 = input("Enter  1st Marks:")
+# marks.append(f1)
+
+# f2 = input("Enter 2nd Marks:")
+# marks.append(f2)
+
+# f3 = input("Enter 3rd Marks:")
+# marks.append(f3)
+
+# f4 = input("Enter 4th Marks:")
+# marks.append(f4)
+
+
+# marks.sort()
+# print(marks)
+
+
+
+
+
+# =======================================================================
+
+# 3. Check that a tuple type cannot be changed in python.
+
+
+# a = (1,2,"tasin",4)
+# a[3] = 3
+# print(a)
+
+
+
+
+
+
+# =======================================================================
+
 # 4. Write a program to sum a list with 4 numbers.
 # 5. Write a program to count the number of zeros in the following tuple:
 # a = （7,0,8,0,0,9）
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,15 @@
-ahammed
+fruits = []
+
+# f1 = input("Enter Fruit 1st name:")
+# fruits.append(f1)
+
+# f2 = input("Enter Fruit 2nd name:")
+# fruits.append(f2)
+
+# f3 = input("Enter Fruit 3rd name:")
+# fruits.append(f3)
+
+# f4 = input("Enter Fruit 4th name:")
+# fruits.append(f4)
+
+# print(fruits)
