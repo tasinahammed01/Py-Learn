c06cee384b0651d2ff32c45caeea2cd1bea08543
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-30 23:31:03 +0600

    list related que added

---
+++ list_Tuple_Practice.py
@@ -0,0 +1,7 @@
+# 1. Write a program to store seven fruits in alist entered by the user.
+# 2. Write a program to accept marks of 6 students and display them in a sorted
+# manner.
+# 3. Check that a type cannot be changed in python.
+# 4. Write a program to sum a list with 4 numbers.
+# 5. Write a program to count the number of zeros in the following tuple:
+# a = （7,0,8,0,0,9）
